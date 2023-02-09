8363ad53b6f71c9f8acc90c7d0a4b0b8886fba06
MehKad <mehkadiri@gmail.com>
2023-02-09 11:08:02 +0000

    Logout

--- StudentManagement/components/screens/Profile.js
+++ StudentManagement/components/screens/Profile.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { getAuth, signOut } from "firebase/auth";
 
 export default class Profile extends Component {
+  onLogOut = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        console.log("Signed out");
+      })
+      .catch((error) => alert(error));
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Profile</Text>
+        <TouchableOpacity style={styles.button} onPress={() => this.onLogOut()}>
+          <Text style={{ color: "#fff", fontWeight: "bold" }}>Logout</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -16,5 +28,17 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  button: {
+    backgroundColor: "#004AAD",
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#fff",
+    paddingBottom: 15,
+    paddingTop: 15,
+    paddingLeft: 120,
+    paddingRight: 120,
+    marginTop: "5%",
+    marginBottom: "10%",
   },
 });
