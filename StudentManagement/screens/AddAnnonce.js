582defdb649db20d6f0fd1fcb37edccb825f2b0f
MehKad <mehkadiri@gmail.com>
2023-02-25 09:49:09 +0000

    show teacher annonce

--- StudentManagement/screens/AddAnnonce.js
+++ StudentManagement/screens/AddAnnonce.js
@@ -1,33 +1,92 @@
 import React, { Component } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StatusBar, StyleSheet, Text, View } from "react-native";
 import { connect } from "react-redux";
 
 class AddAnnonce extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        const { annonces } = this.props;
-        console.log(annonces);
-        return (
-            <View style={styles.container}>
-                <Text>Add Annonce</Text>
-            </View>
-        );
-    }
+  render() {
+    const { annonces } = this.props;
+    console.log(annonces);
+    return (
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={{ alignItems: "center" }}
+          ListHeaderComponentStyle={styles.listheader}
+          ListHeaderComponent={<Text style={styles.header}>Annonce</Text>}
+          data={annonces}
+          renderItem={({ item }) => {
+            const d = new Date(item.date.seconds * 1000);
+            var dd =
+              d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear();
+            return (
+              <View style={styles.posts}>
+                <Text style={styles.title}>{item.title}</Text>
+                <Text style={styles.body}>{item.content}</Text>
+                <Text style={styles.time}>{dd}</Text>
+              </View>
+            );
+          }}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+  container: {
+    marginTop: StatusBar.currentHeight,
+    backgroundColor: "#f0ffff",
+  },
+  listheader: {
+    height: 80,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  posts: {
+    width: 330,
+    justifyContent: "center",
+    backgroundColor: "#386BF6",
+    borderRadius: 20,
+    padding: 15,
+    margin: 15,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12,
     },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 35,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 15,
+    marginBottom: 15,
+    marginTop: 5,
+    textAlign: "center",
+    color: "white",
+  },
+  body: {
+    textAlign: "justify",
+    padding: 5,
+  },
+  time: {
+    textAlign: "right",
+    paddingTop: 10,
+    fontSize: 10,
+    color: "white",
+  },
 });
 
 const mapStateToProps = (store) => ({
-    annonces: store.userState.annonces
-})
+  annonces: store.userState.annonces,
+});
 
 export default connect(mapStateToProps, null)(AddAnnonce);
