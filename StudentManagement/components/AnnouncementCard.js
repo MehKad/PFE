73d140d273e9b742e551b3049949f51eee6f0dbd
imad <imadidaliouali@gmail.com>
2023-03-10 13:25:41 +0100

    style the announcement card

--- StudentManagement/components/AnnouncementCard.js
+++ StudentManagement/components/AnnouncementCard.js
@@ -1,62 +1,137 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Animated } from 'react-native';
+import { MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 
-const AnnouncementCard = (props) => {
-    const { title, content, avatar, date } = props;
+const AnnouncementCard = ({ id, title, content, date, avatar, teacherName, teacher, deleteAnnonce }) => {
+    const [expanded, setExpanded] = useState(false);
+    const animation = useRef(new Animated.Value(0)).current;
+
+    const toggleCard = () => {
+        if (expanded) {
+            Animated.timing(animation, {
+                toValue: 0,
+                duration: 300,
+                useNativeDriver: false,
+            }).start(() => setExpanded(false));
+        } else {
+            Animated.timing(animation, {
+                toValue: 1,
+                duration: 300,
+                useNativeDriver: false,
+            }).start(() => setExpanded(true));
+        }
+    };
+
+    const heightInterpolate = animation.interpolate({
+        inputRange: [0, 1],
+        outputRange: [70, 200],
+    });
+
+    const opacityInterpolate = animation.interpolate({
+        inputRange: [0, 0.5, 1],
+        outputRange: [0, 0.2, 1],
+    });
+
+    const EditButton = () => {
+        return (
+            <TouchableOpacity onPress={() => console.log('Edit announcement')} style={styles.editButton}>
+                <MaterialIcons name="edit" size={20} color="#1c1c1c" />
+            </TouchableOpacity>
+        );
+    }
+
+    const DeleteButton = () => {
+        return (
+            <TouchableOpacity onPress={() => deleteAnnonce(id)} style={styles.deleteButton}>
+                <MaterialCommunityIcons name="delete-outline" size={20} color="#1c1c1c" />
+            </TouchableOpacity>
+        );
+    }
+
     return (
-        <View style={styles.cardContainer}>
-            <Image
-                source={{ uri: avatar }}
-                style={styles.avatarImage}
-            />
-            <View style={styles.textContainer}>
-                <Text style={styles.title} numberOfLines={1}>{title}</Text>
-                <Text style={styles.content} numberOfLines={2}>{content}</Text>
-            </View>
-            <View style={styles.dateContainer}>
-                <Text style={styles.date}>{date}</Text>
-            </View>
-        </View>
+        <TouchableOpacity onPress={toggleCard} activeOpacity={0.8}>
+            <Animated.View style={[styles.container, { height: heightInterpolate }]}>
+                <View style={styles.header}>
+                    <Image style={styles.avatar} source={{ uri: avatar }} />
+                    <View style={styles.headerText}>
+                        <Text style={styles.teacherName}>{teacherName}</Text>
+                        <Text style={styles.date}>{date}</Text>
+                    </View>
+                    {teacher ? <EditButton /> : null}
+                    {teacher ? <DeleteButton /> : null}
+                </View>
+                <Animated.View style={{ opacity: opacityInterpolate }}>
+                    <View style={styles.body}>
+                        <Text style={styles.title}>{title}</Text>
+                        <Text style={styles.content}>{content}</Text>
+                    </View>
+                </Animated.View>
+            </Animated.View>
+        </TouchableOpacity>
     );
 };
 
 const styles = StyleSheet.create({
-    cardContainer: {
+    container: {
+        backgroundColor: '#ffffff',
+        borderRadius: 8,
+        padding: 16,
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#dadce0',
+        elevation: 0, // remove the shadow
+        overflow: 'hidden',
+    },
+    header: {
         flexDirection: 'row',
         alignItems: 'center',
-        padding: 10,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        elevation: 2,
-        marginBottom: 10,
+        marginBottom: 12, // increase the margin
     },
-    avatarImage: {
-        width: 40,
+    avatar: {
+        width: 40, // decrease the size
         height: 40,
         borderRadius: 20,
-        marginRight: 10,
+        marginRight: 12, // decrease the margin
     },
-    textContainer: {
+    headerText: {
         flex: 1,
     },
-    title: {
+    teacherName: {
         fontSize: 16,
         fontWeight: 'bold',
-        marginBottom: 5,
-    },
-    content: {
-        fontSize: 14,
-        marginBottom: 5,
-        color: '#555',
-    },
-    dateContainer: {
-        backgroundColor: '#f1f3f4',
-        borderRadius: 10,
-        padding: 5,
+        color: '#1c1c1c',
+        marginBottom: 4,
     },
     date: {
         fontSize: 12,
-        color: '#555',
+        color: '#5f6368', // change the color
+    },
+    body: {
+        paddingTop: 12,
+        borderTopWidth: 1,
+        borderTopColor: '#dadce0', // change the color
+    },
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: '#1a73e8', // change the color
+        marginBottom: 8,
+    },
+    content: {
+        fontSize: 16,
+        color: '#202124', // change the color
+    },
+    editButton: {
+        backgroundColor: '#e0e0e0',
+        borderRadius: 4,
+        padding: 4,
+        marginLeft: 12,
+    },
+    deleteButton: {
+        backgroundColor: '#ffcdd2',
+        borderRadius: 4,
+        padding: 4,
+        marginLeft: 8,
     },
 });
 
